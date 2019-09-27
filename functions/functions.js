1c1f28c05ebd26e49c2355f143db80cb9d37756e
tad1693 <tadmhi@gmail.com>
2019-09-27 16:18:05 -0600

    Firestore implementation

--- functions/functions.js
+++ functions/functions.js
@@ -8,10 +8,12 @@ exports.findDepartmentByName = departmentName => {
     return db.collection('Departments').where('name', '==', departmentName.toUpperCase())
         .get()
         .then(snapshot => {
-            console.log(snapshot.size);
-            snapshot.forEach(doc => console.log(doc.data()));
+            // console.log(snapshot.size);
+            return snapshot[0].data();
         })
         .catch(error => {
             console.log('Error getting documents: ', error)
         })
 }
+
+// exports.findDepartmentByName('Granada')
