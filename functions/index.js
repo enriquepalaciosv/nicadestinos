d3b9f137a38e51229af2d82a7d84b8b00a1b6dbd
Enrique Palacios <enrique.palacios@struck.com>
2019-09-27 14:47:58 -0600

    Add ani test intent

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,18 @@
-const { dialogflow } = require('actions-on-google');
+const { dialogflow, Suggestions } = require('actions-on-google');
 const functions = require('firebase-functions');
 
 const app = dialogflow({ debug: true });
 
-app.intent('intent-name', (conv, parameters) => {
-    return conv.ask('¿Que puedo hacer por ti?');
+app.intent('Default Welcome Intent', (conv, parameters) => {
+    conv.ask('Hola, bienvenido a Nica Destinos, puedo ayudar a encontrar destinos en Esteli, Matagalpa, Granada y Rivas');
+    conv.ask('¿Que departamento deseas visitar en Nicaragua?');
+    conv.ask(new Suggestions('Esteli', 'Matagalpa'));
+    _
+});
+
+
+app.intent('Ani Test Intent', (conv, { Departamento }) => {
+
 });
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
