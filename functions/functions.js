e7c6d4d8dc1606e81e38de66487836415dad1f4a
tad1693 <tadmhi@gmail.com>
2019-09-27 15:32:57 -0600

    Deploy firebase function

--- functions/functions.js
+++ functions/functions.js
@@ -1,19 +1,19 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
-process.env.DEBUG = 'dialogflow:debug';
+process.env.DEBUG = 'dialogflow:debug'
 
-admin.initializeApp(functions.config().firebase);
-const db = admin.firestore();
+admin.initializeApp(functions.config().firebase)
+const db = admin.firestore()
 
 exports.aniTest = Departmento => {
-    db.collection("departments").where("name", "==", Departmento.toLowerCase())
-        .get()
-        .then((querySnapshot) => {
-            console.log(querySnapshot.data());
-            return querySnapshot.data();
-        })
-        .catch((error) => {
-            console.log("Error getting documents: ", error);
-        });
-};
+  db.collection('departments').where('name', '==', Departmento.toLowerCase())
+    .get()
+    .then((querySnapshot) => {
+      console.log(querySnapshot.data())
+      return querySnapshot.data()
+    })
+    .catch((error) => {
+      console.log('Error getting documents: ', error)
+    })
+}
--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,19 @@
-const { aniTest } = require('./functions');
-const { dialogflow, Suggestions } = require('actions-on-google');
-const functions = require('firebase-functions');
-const app = dialogflow({ debug: true });
+'use strict'
+const {aniTest} = require('./functions')
+const {dialogflow, Suggestions} = require('actions-on-google')
+const functions = require('firebase-functions')
+const app = dialogflow({debug: true})
 
-app.intent('Default Welcome Intent', (conv, parameters) => {
-    conv.ask('Hola, bienvenido a Nica Destinos, puedo ayudar a encontrar destinos en Esteli, Matagalpa, Granada y Rivas');
-    conv.ask('¿Que departamento deseas visitar en Nicaragua?');
-    conv.ask(new Suggestions('Esteli', 'Matagalpa'));
-    _
-});
+app.intent('Default Welcome Intent', conv => {
+  conv.ask('Hola, bienvenido a Nica Destinos, puedo ayudar a encontrar destinos en Esteli, Matagalpa, Granada y Rivas')
+  conv.ask('¿Que departamento deseas visitar en Nicaragua?')
+  conv.ask(new Suggestions('Esteli', 'Matagalpa'))
+})
 
 
-app.intent('Ani Test Intent', async(conv, { Departamento }) => {
-    const department = await aniTest(Departamento);
-    console.log('Ani Test Intent', department);
-    conv.ask('Result: ' + department.description);
-});
+app.intent('Ani Test Intent', async (conv, {Departamento}) => {
+  const department = await aniTest(Departamento)
+  conv.ask('Result: ' + department.description)
+})
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+exports.fulfillment = functions.https.onRequest(app);
