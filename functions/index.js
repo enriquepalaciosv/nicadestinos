d9cba5831ea4dd100a5b141401a58a8603719b16
Enrique Palacios <enrique.palacios@struck.com>
2019-09-27 12:11:08 -0600

    Add fulfillment boilerplate

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,10 @@
+const { dialogflow } = require('actions-on-google');
 const functions = require('firebase-functions');
+
+const app = dialogflow({ debug: true });
+
+app.intent('intent-name', (conv, parameters) => {
+    return conv.ask('¿Que puedo hacer por ti?');
+});
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
