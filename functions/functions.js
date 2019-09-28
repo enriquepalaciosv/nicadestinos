94b13936c0a0eb3df332db3d5f0969e7b48266be
Enrique Palacios <enrique.palacios@struck.com>
2019-09-27 23:27:40 -0600

    Add capitalize util

--- functions/functions.js
+++ functions/functions.js
@@ -4,12 +4,18 @@ process.env.DEBUG = 'dialogflow:debug'
 
 const db = admin.firestore();
 
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
 exports.findAllDepartments = () => {
     return db.collection('Departments').get()
         .then(snapshot => {
             const departments = [];
-            snapshot.forEach(d => departments.push(d.data()));
-            console.log(...departments.map(d => d.name));
+            snapshot.forEach(d => {
+                const data = d.data();
+                departments.push({...data, name: capitalize(data.name) });
+            });
             return departments;
         })
         .catch(error => {
@@ -17,6 +23,8 @@ exports.findAllDepartments = () => {
             return [];
         })
 };
+exports.findAllDepartments();
+
 
 exports.findDepartmentByName = departmentName => {
     return db.collection('Departments').where('name', '==', departmentName.toUpperCase())
@@ -34,13 +42,12 @@ exports.findDepartmentByName = departmentName => {
 
 exports.getInlineEnum = names => {
     let inline = "";
-    names.forEach((d, i) => {
-        inline += (i + 1 == names.length) ? `y ${d}` : `${d}, `;
-    });
-    console.log(inline);
+    if (names.length === 1) {
+        inline = names[0];
+    } else {
+        names.forEach((d, i) => {
+            inline += (i + 1 === names.length) ? `y ${d}` : (i === names.length - 2 ? `${d} ` : `${d}, `);
+        });
+    }
     return inline;
 };
-
-//exports.findDepartmentByName('Granada');
-//exports.findAllDepartments();
-//exports.getSpanishReadableEnum(['uno', 'dos', 'tres', 'cuatro']);
